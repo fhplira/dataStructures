b3a364e6a686fc511c1f38e31637ea18b283ba12
Fernanda Helen <fhplira@gmail.com>
2024-05-13 15:00:19 -0300

    adicao das interfaces Tree e BinaryTree

---
+++ src/main/java/com/projects/ade/BinaryTree.java
@@ -0,0 +1,14 @@
+package com.projects.ade;
+
+public interface BinaryTree extends Tree{
+
+    //Pre-order route
+    public void preOrderTraversal();
+
+    //In-order route
+    public void inOrderTraversal();
+
+    //Post-order route
+    public void postOrderTraversal();
+
+}
---
+++ src/main/java/com/projects/ade/Tree.java
@@ -0,0 +1,14 @@
+package com.projects.ade;
+
+public interface Tree {
+
+    //Adds an element to the tree
+    public void add(int element);
+
+    //Checks if an element is present in the tree
+    public boolean contains(int element);
+
+    //Removes an element from the tree
+    public void remove(int element);
+
+}
